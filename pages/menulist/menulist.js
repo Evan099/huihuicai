dc49d7396a1c9abced00a0bd5986e2d4a01d4f15
Evan099 <891837367@qq.com>
2021-01-19 22:43:21 +0800

    lession 01

--- pages/menulist/menulist.js
+++ pages/menulist/menulist.js
@@ -7,8 +7,36 @@ Page({
   data: {
     msg:"hello",
     value:"",
+    list:[
+      {
+        id:0,
+        name:"猪八戒"
+      },
+      {
+        id:2,
+        name:"唐僧"
+      },
+      {
+        id:2,
+        name:"沙和尚"
+      },
+    ],
+    num:0,
+    num2:0
   },
-
+  handleInput(e){
+    console.log(e.detail.value);
+    this.setData({
+      num:e.detail.value
+    })
+  },
+  handletap(e){
+    console.log(e)
+    const opetation = e.currentTarget.dataset.operation
+    this.setData({
+      num2:this.data.num2+opetation
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
